b12c1fbca72e21d8f52e8f99701ad471e04ed16a
gamnyam <nunice2080@gmail.com>
2024-06-26 00:23:01 +0900

    Update 공배수.py

--- 프로그래머스/0/181936. 공배수/공배수.py
+++ 프로그래머스/0/181936. 공배수/공배수.py
@@ -5,3 +5,16 @@ def solution(number, n, m):
             answer = 0
     
     return answer
+
+# 다른 풀이
+def solution(number, n, m):
+    if number % n == 0 :
+        if number % m == 0 :
+            answer = 1
+        else :
+            answer = 0
+    elif number % n == 0 & number % m == 0 :
+        answer = 1
+    else :
+        answer = 0
+    return answer
