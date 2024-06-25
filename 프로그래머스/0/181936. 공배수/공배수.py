81f88758e67c3d0ab6030d7a99996ea4a61fddb3
gamnyam <nunice2080@gmail.com>
2024-06-26 00:21:13 +0900

    [level 0] Title: 공배수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.py
+++ 프로그래머스/0/181936. 공배수/공배수.py
@@ -1,10 +1,7 @@
 def solution(number, n, m):
-    if number % n == 0 :
-        if number % m == 0 :
-            answer = 1
-        else :
-            answer = 0
+    if number % n == 0 and number % m == 0:
+        answer = 1
     else :
-        answer = 0
+            answer = 0
     
     return answer
