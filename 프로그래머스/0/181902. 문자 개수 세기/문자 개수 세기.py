0fb9c3bc46a8975e9029ce27a448d94e1acb3fbb
gamnyam <nunice2080@gmail.com>
2024-06-29 02:09:01 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = []
+    alp = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    for i in alp:
+        n = my_string.count(i)
+        answer.append(n)
+    
+    return answer
