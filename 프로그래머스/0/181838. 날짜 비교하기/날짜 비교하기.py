e8164c31f3644d234a576e0c2d9ee602c6203df2
gamnyam <nunice2080@gmail.com>
2024-06-30 12:39:30 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,18 @@
+def solution(date1, date2):
+    # 연도 비교
+    if date1[0] < date2[0]:
+        return 1
+    elif date1[0] > date2[0]:
+        return 0
+    
+    # 월 비교
+    if date1[1] < date2[1]:
+        return 1
+    elif date1[1] > date2[1]:
+        return 0
+    
+    # 일 비교
+    if date1[2] < date2[2]:
+        return 1
+    else:
+        return 0
