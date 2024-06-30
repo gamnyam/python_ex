689f218453dedfbad77af289f1cea11ea5766a0a
gamnyam <nunice2080@gmail.com>
2024-06-30 23:28:38 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 32.31 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
@@ -1,7 +1,7 @@
 def solution(arr, queries):
     answer = arr
-    for s, e in queries :
-        for i in range(s,e+1):
+    for i in range(len(arr)):
+        for s, e in queries :
             if s <= i and i<= e:
                 arr[i]+=1
     return answer
