e462f456e467493598af099b795fa4210c0fb3bb
gamnyam <nunice2080@gmail.com>
2024-07-01 20:30:46 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 0.18 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -0,0 +1,12 @@
+def solution(arr, k):
+    answer = []
+    seen = set()
+    for i in arr:
+        if i not in seen:
+            answer.append(i)
+            seen.add(i)
+        if len(answer)==k:
+            break
+    while len(answer)<k:
+        answer.append(-1)
+    return answer
