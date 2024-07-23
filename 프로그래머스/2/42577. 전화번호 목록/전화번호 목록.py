5fb7cc1451c7f8b2f045df8ec8290b3f3d6e09ae
gamnyam <nunice2080@gmail.com>
2024-07-23 16:51:58 +0900

    [level 2] Title: 전화번호 목록, Time: 108.60 ms, Memory: 30.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,8 @@
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i,phone in enumerate(phone_book[:-1]):
+        target_phone = phone_book[i+1]
+        if target_phone[:len(phone)] == phone :
+            return False
+    return answer
