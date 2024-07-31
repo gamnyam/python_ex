d27f993e4c6067f2a800a61ab213e85efb787d31
gamnyam <nunice2080@gmail.com>
2024-07-31 15:24:53 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -0,0 +1,9 @@
+def solution(x):
+    x = str(x)
+    sum = 0
+    for i in range(len(x)):
+        sum += int(x[i])
+    if int(x) % sum == 0:
+        return True
+    else : 
+        return False
