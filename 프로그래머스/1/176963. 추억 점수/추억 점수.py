055aafeb7ec9d8e68cf394a1be315157556fc1c5
gamnyam <nunice2080@gmail.com>
2024-10-22 13:50:27 +0900

    [level 1] Title: 추억 점수, Time: 1.96 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,11 +1,10 @@
 def solution(name, yearning, photo):
-    
     answer = []
     name2score = dict(zip(name, yearning))
     
-    for names in photo:
+    for names in photo :
         score = 0
-        for name in names :
-            score += name2score.get(name,0)
+        for name in names:
+            score += name2score.get(name, 0)
         answer.append(score)
     return answer
